482b7d6e977d9a39c4c9878e26713d9f29bb206b
zhaoyang <zhao_volar@163.com>
2022-11-05 19:30:53 +0800

    solidity code

---
+++ SolidityStudy/code/AbiDecode.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract AbiDecode {
+    struct Mystruct {
+        string mame;
+        uint[2] nums;
+    }
+
+    function encode (uint x, address addr, uint[] calldata arr, Mystruct calldata myStruct) external pure returns (bytes memory) {
+        return abi.encode(x, addr, arr, myStruct);
+    }
+
+    function decode(bytes calldata data) external pure returns (uint x, address addr, uint[] memory arr, Mystruct memory myStruct) {
+        (x, addr, arr, myStruct) = abi.decode(data, (uint, address, uint[], Mystruct));
+    }
+}
