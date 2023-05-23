1b0d355a1012b3dc16d5fc71e95bb148f4441fdd
zhaoyang <zhao_volar@163.com>
2023-05-24 00:31:10 +0800

    update method of insertIntoClose and insertIntoFar

--- GoStudy/project/K_Bucket/main.go
+++ GoStudy/project/K_Bucket/main.go
@@ -14,7 +14,7 @@ type Node struct {
 }
 
 type Bucket struct {
-	ids            [3]*Node
+	ids            []*Node
 	lchild, rchild int
 }
 
@@ -36,7 +36,7 @@ func (s *Node) FindNode(id string) (*Node, *Node) {
 	//对桶中的节点进行遍历看是否有目标节点，如果没有的话返回随机的两个节点信息
 	for _, v := range a.ids {
 		if v.nodeID == id {
-			return nil, nil
+			return v, v
 		}
 	}
 	//获取桶中最近的两个节点并返回
@@ -45,7 +45,7 @@ func (s *Node) FindNode(id string) (*Node, *Node) {
 	return a.ids[index1], a.ids[index2]
 }
 
-// 生成两个随机数
+// 生成两个随机数，0~2之间
 func GetRandom2() (int, int) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	// 随机生成两个不重复的整数
@@ -77,29 +77,78 @@ func GetRandom2() (int, int) {
 */
 
 func (s *Node) InsertNode(nodeId string) bool {
+	new_node := Node{nodeID: nodeId}
 	result := findBucket(s.nodeID, nodeId)
 	if result < 0 {
 		return false
 	}
 
-	var bucket *Bucket
+	var index int
 	if result >= len(s.buckets)-1 {
-		bucket = s.buckets[len(s.buckets)-1]
-		insertIntoClose(bucket)
+		index = len(s.buckets) - 1
+		insertIntoClose(index, &new_node, s)
 	} else {
-		bucket = s.buckets[result]
-		isnertIntoFar(bucket)
+		index = result
+		isnertIntoFar(index, &new_node, s)
 	}
 
 	return true
 }
 
-func insertIntoClose(bucket *Bucket) {
+func insertIntoClose(index int, new_node *Node, target_node *Node) {
+	bucket := target_node.buckets[index]
+	//判断桶是否已满，没满的话加入桶中，满的话进行扩充
+	if len(bucket.ids) < 3 {
+		bucket.ids = append(bucket.ids, new_node)
+	} else {
+		//如果桶的数量大于160个的话就不会进行分裂
+		if len(target_node.buckets) >= 160 {
+			return
+		}
+		bucket_far := Bucket{}
+		bucket_near := Bucket{}
+		//将所有节点放到一起
+		nodes := bucket.ids
+		nodes = append(nodes, new_node)
+		//将每个节点的距离计算出来并加入数组之中
+		var distance []int
+		for i, v := range nodes {
+			num1, _ := strconv.Atoi(v.nodeID)
+			num2, _ := strconv.Atoi(target_node.nodeID)
+			distance[i] = num1 ^ num2
+		}
 
+		//对距离数组进行筛选，选出最近的1个节点加入最远桶
+		temp := distance[0]
+		index_max := 0
+		for i := 1; i < len(distance); i++ {
+			if distance[i] > temp {
+				temp = distance[i]
+				index_max = i
+			}
+		}
+
+		//将最远的节点加入最远桶，最近的加入最近桶
+		bucket_far.ids = append(bucket_far.ids, nodes[index_max])
+		for i, _ := range distance {
+			if i == index_max {
+				continue
+			}
+			bucket_near.ids = append(bucket_near.ids, nodes[i])
+		}
+
+		//将bucket进行更新
+		target_node.buckets[index] = &bucket_far
+		target_node.buckets = append(target_node.buckets, &bucket_near)
+	}
 }
 
-func isnertIntoFar(bucket *Bucket) {
-
+func isnertIntoFar(index int, new_node *Node, target_node *Node) {
+	bucket := target_node.buckets[index]
+	//小于三个就更新，满了就不管（简化），事实上要进行心跳监测
+	if len(bucket.ids) < 3 {
+		bucket.ids = append(bucket.ids, new_node)
+	}
 }
 
 func findBucket(selfId, tragetId string) int {
