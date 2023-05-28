af14eb4b757f70389d50790eae68bca5703c4b89
zhaoyang <zhao_volar@163.com>
2023-05-28 10:20:15 +0800

    finally version

--- GoStudy/project/K_Bucket/main.go
+++ GoStudy/project/K_Bucket/main.go
@@ -21,16 +21,21 @@ func initNode(nodeId string) {
 
 func (s *Node) FindNode(nodeID string, array []*Node) []*Node {
 	var nodes []*Node
+	var return_node []*Node
 	// var isUpdate bool
 	if s.nodeID == nodeID {
-		return nil
+		nodes = append(nodes, s, s)
+		return nodes
+	}
+
+	if len(s.buckets) == 0 {
+		return array
 	}
 
 	//寻找到对应的桶
 	result := findBucket(s.nodeID, nodeID)
 	var bucket *Bucket
 	if result >= (len(s.buckets) - 1) {
-		result = len(s.buckets) - 1
 		bucket = s.buckets[len(s.buckets)-1]
 	} else {
 		bucket = s.buckets[result]
@@ -39,38 +44,166 @@ func (s *Node) FindNode(nodeID string, array []*Node) []*Node {
 	//判断桶中是否存在该节点
 	for _, v := range bucket.ids {
 		if v.nodeID == nodeID {
-			return nil
+			nodes = append(nodes, s, s)
+			return nodes
 		}
 	}
 
 	var node1, node2 *Node
+	var nodeNum int
 	//不存在就进行递归,桶中选取随机的两个节点
 	if len(bucket.ids) == 2 {
+		node1 = bucket.ids[0]
+		node2 = bucket.ids[1]
+		nodeNum = 2
+	} else if len(bucket.ids) == 1 {
+		node1 = bucket.ids[0]
+		nodeNum = 1
+	} else if len(bucket.ids) > 2 {
 		index1, index2 := GetRandom2()
 		node1 = bucket.ids[index1]
 		node2 = bucket.ids[index2]
-	} else if len(bucket.ids) == 1 {
-		node1 = bucket.ids[0]
-	} 
+		nodeNum = 2
+	}
 
 	//判断两个新选取的节点的距离与传入的节点的距离相比
 	//如果找不到比传入节点更近的节点，寻找就结束（找不到比传入更近的）
 	//如果找到的话执行FindNode，对更新的节点进行查找
-	if 
-	nodes = append(nodes, node1, node2)
+	if nodeNum == 2 {
+		//在第一遍比较的时候，对于array中已经发生交换的元素，在第二次比较的时候就会进行跳过
+		isUpdate := -1
+		for i, v := range array {
+			result := compareGetMin(nodeID, v.nodeID, node1.nodeID)
+			if result == node1.nodeID {
+				array[i] = node1
+				isUpdate = i
+				return_node = append(return_node, node1.FindNode(nodeID, array)...)
+			}
+		}
+
+		for i := len(array) - 1; i >= 0; i-- {
+			if i == isUpdate {
+				continue
+			}
+			result := compareGetMin(nodeID, array[i].nodeID, node2.nodeID)
+			if result == node2.nodeID {
+				array[i] = node2
+				return_node = append(return_node, node2.FindNode(nodeID, array)...)
+			}
+		}
+	} else if nodeNum == 1 {
+		num := new(big.Int)
+		num1 := new(big.Int)
+		num2 := new(big.Int)
+		num3 := new(big.Int)
+		num.SetString(nodeID, 2)
+		num1.SetString(array[0].nodeID, 2)
+		num2.SetString(array[1].nodeID, 2)
+		num3.SetString(node1.nodeID, 2)
+		//选出array中最大的与新结点进行比较
+		result1 := new(big.Int)
+		result1.Xor(num, num1)
+		result2 := new(big.Int)
+		result2.Xor(num, num2)
+
+		if result1.Cmp(result2) > 0 {
+			if num3.Cmp(num1) < 0 {
+				array[0] = node1
+			}
+			return_node = append(return_node, node1.FindNode(nodeID, array)...)
+		} else if result1.Cmp(result2) < 0 {
+			if num3.Cmp(num2) < 0 {
+				array[1] = node1
+			}
+			return_node = append(return_node, node1.FindNode(nodeID, array)...)
+		}
+	} else {
+		return array
+	}
 
 	//将新节点FindNode返回的列表中的节点与传入的列表中的节点进行比对，选出最近的两个节点进行返回
+	//将返回的节点信息中与array中不相同的节点加入array中
+	for _, v := range return_node {
+		if v.nodeID == array[0].nodeID || v.nodeID == array[1].nodeID {
+			continue
+		}
+		array = append(array, v)
+	}
+
+	//如果array的长度小于等于2的话，说明找到最小的2个节点或1一个节点的信息
+	if len(array) <= 2 {
+		return array
+	}
+	//否则就挑选出arry中距离最小的两个元素放入nodes中
+	index_min1, index_min2 := get2MinIndex(array, nodeID)
+	nodes = append(nodes, array[index_min1], array[index_min2])
 
 	return nodes
 }
 
+func compareGetMin(targetValue, value1, value2 string) string {
+	num := new(big.Int)
+	num1 := new(big.Int)
+	num2 := new(big.Int)
+	num.SetString(targetValue, 2)
+	num1.SetString(value1, 2)
+	num2.SetString(value2, 2)
+	//计算出距离
+	result1 := new(big.Int)
+	result1.Xor(num, num1)
+	result2 := new(big.Int)
+	result2.Xor(num, num2)
+
+	if result1.Cmp(result2) < 0 {
+		return value1
+	} else {
+		return value2
+	}
+}
+
+func get2MinIndex(nodes []*Node, targetNode string) (int, int) {
+	var distance []*big.Int
+	for _, v := range nodes {
+		num1 := new(big.Int)
+		num2 := new(big.Int)
+		num1.SetString(targetNode, 2)
+		num2.SetString(v.nodeID, 2)
+		result := new(big.Int)
+		result.Xor(num1, num2)
+		distance = append(distance, result)
+	}
+
+	//找出aray中最近的两个元素
+	temp1 := distance[0]
+	index_min1 := 0
+	temp2 := distance[1]
+	index_min2 := 1
+	//先找到第一个最近的
+	for i := 1; i < len(distance); i++ {
+		if distance[i].Cmp(temp1) < 0 {
+			temp1 = distance[i]
+			index_min1 = i
+		}
+	}
+	//在找到第二个近的
+	for i, v := range distance {
+		if i == index_min1 || i == 1 {
+			continue
+		}
+		if v.Cmp(temp2) < 0 {
+			temp2 = distance[i]
+			index_min2 = i
+		}
+	}
+	return index_min1, index_min2
+}
 
 // 生成两个随机数，0~2之间
 func GetRandom2() (int, int) {
 	var nums [2]int
 	// 随机生成两个不重复的整数
 	for i := range nums {
-		num, err := rand.Int(rand.Reader, big.NewInt(10))
+		num, err := rand.Int(rand.Reader, big.NewInt(3))
 		if err != nil {
 			// 处理错误
 			return -1, -1
@@ -78,7 +211,7 @@ func GetRandom2() (int, int) {
 		nums[i] = int(num.Int64())
 	}
 	for nums[0] == nums[1] {
-		num, err := rand.Int(rand.Reader, big.NewInt(10))
+		num, err := rand.Int(rand.Reader, big.NewInt(3))
 		if err != nil {
 			// 处理错误
 			fmt.Println(err)
@@ -124,32 +257,32 @@ func (s *Node) InsertNode(nodeId string) bool {
 		insertIntoClose(index, &new_node, s)
 	} else {
 		index = result
-		isnertIntoFar(index, &new_node, s)
+		insertIntoFar(index, &new_node, s)
 	}
 	return true
 }
 
-func insertIntoClose(index int, new_node *Node, target_node *Node) {
-	bucket := target_node.buckets[index]
+func insertIntoClose(index int, newNode *Node, targetNode *Node) {
+	bucket := targetNode.buckets[index]
 	//判断桶中是否已经存在要加入的节点
 	for _, v := range bucket.ids {
-		if v.nodeID == new_node.nodeID {
+		if v.nodeID == newNode.nodeID {
 			return
 		}
 	}
 	//判断桶是否已满，没满的话加入桶中，满的话进行扩充
 	if len(bucket.ids) < 3 {
-		bucket.ids = append(bucket.ids, new_node)
+		bucket.ids = append(bucket.ids, newNode)
 	} else {
 		//如果桶的数量大于160个的话就不会进行分裂
-		if len(target_node.buckets) >= 160 {
+		if len(targetNode.buckets) >= 160 {
 			return
 		}
 		bucket_far := Bucket{}
 		bucket_near := Bucket{}
 
 		//将所有节点放到一起
-		bucket.ids = append(bucket.ids, new_node)
+		bucket.ids = append(bucket.ids, newNode)
 		//将每个节点的距离计算出来并加入数组之中
 		var distance []*big.Int
 		for _, v := range bucket.ids {
@@ -158,7 +291,7 @@ func insertIntoClose(index int, new_node *Node, target_node *Node) {
 			num1 := new(big.Int)
 			num2 := new(big.Int)
 			num1.SetString(v.nodeID, 2)
-			num2.SetString(target_node.nodeID, 2)
+			num2.SetString(targetNode.nodeID, 2)
 			xor := new(big.Int)
 			xor.Xor(num1, num2)
 			distance = append(distance, xor)
@@ -183,25 +316,26 @@ func insertIntoClose(index int, new_node *Node, target_node *Node) {
 		}
 
 		//将bucket进行更新
-		target_node.buckets[index] = &bucket_far
-		target_node.buckets = append(target_node.buckets, &bucket_near)
+		targetNode.buckets[index] = &bucket_far
+		targetNode.buckets = append(targetNode.buckets, &bucket_near)
 	}
 }
 
-func isnertIntoFar(index int, new_node *Node, target_node *Node) {
-	bucket := target_node.buckets[index]
+func insertIntoFar(index int, newNode *Node, targetNode *Node) {
+	bucket := targetNode.buckets[index]
 	//查看桶中是否已经存在新的节点
 	for _, v := range bucket.ids {
-		if v.nodeID == new_node.nodeID {
+		if v.nodeID == newNode.nodeID {
 			return
 		}
 	}
 	//小于三个就更新，满了就不管（简化），事实上要进行心跳监测
 	if len(bucket.ids) < 3 {
-		bucket.ids = append(bucket.ids, new_node)
+		bucket.ids = append(bucket.ids, newNode)
 	}
 }
 
+// 寻找属于第几个桶
 func findBucket(selfId, targetId string) int {
 	num1 := new(big.Int)
 	num2 := new(big.Int)
@@ -220,41 +354,7 @@ func (s *Bucket) printBucketContents() {
 	}
 }
 
-func main() {
-	//测试insert方法
-	// 生成100个不重复的160位二进制字符串
-	var binaryStrs []string
-	for len(binaryStrs) < 100 {
-		max := new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 160), big.NewInt(1))
-		// 生成一个160位的随机二进制字符串
-		num, _ := rand.Int(rand.Reader, max)
-		binaryStr := fmt.Sprintf("%0160b", num)
-
-		// 检查这个二进制字符串是否已经存在
-		if !isDuplicate(binaryStr, binaryStrs) {
-			binaryStrs = append(binaryStrs, binaryStr)
-		}
-	}
-
-	node := Node{nodeID: binaryStrs[0]}
-	fmt.Println("nodeID = ", node.nodeID)
-
-	for i, v := range binaryStrs {
-		if i == 0 {
-			continue
-		}
-		node.InsertNode(v)
-	}
-	println("---------------------------------------------------------")
-
-	for i, v := range node.buckets {
-		fmt.Printf("buckets num is = %d \n", i)
-		v.printBucketContents()
-		fmt.Println("--------------------------")
-	}
-
-}
-
+// 查看是否有相同的元素
 func isDuplicate(binaryStr string, binaryStrs []string) bool {
 	// 将二进制字符串转换为大整数类型
 	num := new(big.Int)
@@ -270,4 +370,116 @@ func isDuplicate(binaryStr string, binaryStrs []string) bool {
 	}
 
 	return false
+}
+
+func testInsert() {
+	var binaryStrs []string
+	for len(binaryStrs) < 101 {
+		max := new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 160), big.NewInt(1))
+		// 生成一个160位的随机二进制字符串
+		num, _ := rand.Int(rand.Reader, max)
+		binaryStr := fmt.Sprintf("%0160b", num)
+
+		// 检查这个二进制字符串是否已经存在
+		if !isDuplicate(binaryStr, binaryStrs) {
+			binaryStrs = append(binaryStrs, binaryStr)
+		}
+	}
+
+	node := Node{nodeID: binaryStrs[0]}
+	fmt.Println("nodeID = ", node.nodeID)
+	for i, v := range binaryStrs {
+		if i == 0 {
+			continue
+		}
+		node.InsertNode(v)
+	}
+
+	for i, v := range node.buckets {
+		fmt.Printf("buckets num is = %d \n", i)
+		v.printBucketContents()
+		fmt.Println("--------------------------")
+	}
+}
+
+func testFindNode() {
+	var binaryStrs []string
+	for len(binaryStrs) < 206 {
+		max := new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 160), big.NewInt(1))
+		// 生成一个160位的随机二进制字符串
+		num, _ := rand.Int(rand.Reader, max)
+		binaryStr := fmt.Sprintf("%0160b", num)
+
+		// 检查这个二进制字符串是否已经存在
+		if !isDuplicate(binaryStr, binaryStrs) {
+			binaryStrs = append(binaryStrs, binaryStr)
+		}
+	}
+	//介绍人节点
+	var nodes []*Node
+	//所有的节点
+	var allNodes []*Node
+
+	node0 := Node{nodeID: binaryStrs[0]}
+	node1 := Node{nodeID: binaryStrs[1]}
+	node2 := Node{nodeID: binaryStrs[2]}
+	node3 := Node{nodeID: binaryStrs[3]}
+	node4 := Node{nodeID: binaryStrs[4]}
+
+	nodes = append(nodes, &node0, &node1, &node2, &node3, &node4)
+	allNodes = append(allNodes, nodes...)
+
+	//构建介绍人节点的桶
+	for i := 0; i < len(nodes); i++ {
+		for j := 0; j < 5; j++ {
+			if i == j {
+				continue
+			}
+			nodes[0].InsertNode(binaryStrs[j])
+		}
+	}
+
+	//将200个新结点进行初始化加入网络
+	for i := 5; i < len(binaryStrs); i++ {
+		newNode := Node{nodeID: binaryStrs[i]}
+		//随机选取一个介绍人节点
+		num, err := rand.Int(rand.Reader, big.NewInt(5))
+		if err != nil {
+			// 处理错误
+			return
+		}
+
+		newNode.InsertNode(nodes[num.Int64()].nodeID)
+		byte1 := []byte(binaryStrs[i])
+		for i, v := range byte1 {
+			if v == '0' {
+				byte1[i] = '1'
+			} else {
+				byte1[i] = '0'
+			}
+		}
+		var tempArray []*Node
+		far_node := Node{nodeID: string(byte1)}
+		tempArray = append(tempArray, &far_node, &far_node)
+		nodeIds := nodes[num.Int64()].FindNode(newNode.nodeID, tempArray)
+		for _, v := range nodeIds {
+			newNode.InsertNode(v.nodeID)
+		}
+		allNodes = append(allNodes, &newNode)
+	}
+
+	for _, v := range allNodes {
+		fmt.Println("Node = ", v.nodeID)
+		for k, j := range v.buckets {
+			fmt.Printf("buckets num is = %d \n", k)
+			j.printBucketContents()
+			fmt.Println("-----------------------------------------------------------------")
+		}
+		fmt.Println("********************************************************************************")
+	}
+}
+
+func main() {
+	// testInsert()
+	testFindNode()
 }
